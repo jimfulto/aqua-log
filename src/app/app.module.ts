6849b4e5e7919e9aeb63e971bca5b3f7d14baa99
Jim Fulto <jimfulto233@gmail.com>
2021-11-21 17:42:08 +0530

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { WeatherComponent } from './weather/weather.component';
+import { HeaderComponent } from './header/header.component';
+import { WeatherListComponent } from './weather/weather-list/weather-list.component';
+import { WeatherItemComponent } from './weather/weather-list/weather-item/weather-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    WeatherComponent,
+    HeaderComponent,
+    WeatherListComponent,
+    WeatherItemComponent
   ],
   imports: [
     BrowserModule,
